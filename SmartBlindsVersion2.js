69b084e49b4fca9160d6b7bf32596dec2a38be0c
Boris Evtimov <borisevtimov1980@gmail.com>
2025-12-13 21:51:30 +0200

    Refactor light sensor logic and thresholds
    Updated light thresholds and simplified logic for opening and closing blinds based on light levels.

--- SmartBlindsVersion2.js
+++ SmartBlindsVersion2.js
@@ -1,55 +1,46 @@
+//In version 2 of the project, we use the built-in light sensors on the micro:bit
 let mode = 0  // 0 = OUT, 1 = IN
 let autoMode = false
 
-// Button A → OUT mode 
 input.onButtonPressed(Button.A, function () {
     mode = 0
     autoMode = true
     basic.showString("OUT")
 })
 
-// Button B → IN mode + automatic closing
 input.onButtonPressed(Button.B, function () {
     mode = 1
     autoMode = true
     basic.showString("IN")
-    closeBlinds()   // closes immediately
 })
 
-// Open blinds (servo at 0°)
 function openBlinds() {
     pins.servoWritePin(AnalogPin.P0, 0)
 }
 
-// Close blinds (servo at 80°)
 function closeBlinds() {
     pins.servoWritePin(AnalogPin.P0, 80)
 }
 
 basic.forever(function () {
-    if (!autoMode) return   // if the automatic mode is not enabled → does nothing
+    if (!autoMode) return
 
-    let light = input.lightLevel()  // 0–255, but for real 0–80
+    let light = input.lightLevel()  
 
     
-    let openThreshold = 20
-    let closeThreshold = 35
+    let openThreshold = 5
+    let closeThreshold = 15
 
     if (mode == 0) {
-        // OUT mode
-        if (light > closeThreshold) {
-            closeBlinds()
-        } else if (light < openThreshold) {
-            openBlinds()
-        }
+        // OUT → light = closes, dark = opens
+        if (light > closeThreshold) closeBlinds()
+        else if (light < openThreshold) openBlinds()
     } else {
-        // IN mode
-        if (light < openThreshold) {
-            openBlinds()
-        } else if (light > closeThreshold) {
-            closeBlinds()
-        }
+        // IN → bright = opens, dark = closes
+        if (light > closeThreshold) openBlinds()
+        else if (light < openThreshold) closeBlinds()
     }
 
-    basic.pause(500)
+
+    basic.pause(300)
 })
